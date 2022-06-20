2018cf3adaa2a01a67594405bbbd672500f62787
Daniel <63756498+Abuyedaniel@users.noreply.github.com>
2022-06-20 23:36:31 +0100

    first commit

--- Calculator.js
+++ Calculator.js
@@ -4,14 +4,15 @@
     var num2 = parseInt(prompt("Number 2: "))
 
     if (opr == "+") {
-        console.log(num1 + num2)
+        alert(num1 + num2)
     }
     else if (opr == "-") {
-        console.log(num1 - num2)
+        alert(num1 - num2)
+
     }
     else if (opr == "*") {
-        console.log(num1 * num2)
+        alert(num1 * num2)
     }
     else if (opr == "/") {
-        console.log(num1 / num2)
+        alert(num1 / num2)
     }
