2c3de900da752b9f622909544750ceebfff21e9f
Daniel <63756498+Abuyedaniel@users.noreply.github.com>
2022-06-21 00:15:46 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,18 @@
+//    Without Frontend 
+    var num1 = parseInt(prompt("Number 1: "))
+    var opr = prompt("Operator: ")
+    var num2 = parseInt(prompt("Number 2: "))
+
+    if (opr == "+") {
+        alert(num1 + num2)
+    }
+    else if (opr == "-") {
+        alert(num1 - num2)
+
+    }
+    else if (opr == "*") {
+        alert(num1 * num2)
+    }
+    else if (opr == "/") {
+        alert(num1 / num2)
+    }
