0c7db4f0624cc05e547889bec3357ed56355a06b
Daniel <63756498+Abuyedaniel@users.noreply.github.com>
2022-06-21 18:25:05 +0100

    Create calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
-num1 = parseInt(prompt("Number 1: "));
-opr = prompt("Operator: ");
-num2 = parseInt(prompt("Number 2: "));
-result = "";
+"use strict"
+var num1 = parseInt(prompt("Number 1: "));
+var opr = prompt("Operator: ");
+var num2 = parseInt(prompt("Number 2: "));
+var result = "";
 
 if (opr == "+") {
   result = num1 + num2;
