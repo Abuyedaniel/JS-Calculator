3591586d78bd1fd0384cc1d58af8294f6c461d0d
Abuye Daniel <abuyedaniel18@gmail.com>
2022-06-23 12:04:51 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var num1 = parseInt(prompt("Number 1: "));
+var opr = prompt("Operator: ");
+var num2 = parseInt(prompt("Number 2: "));
+var T = "Thank You";
+
+let result;
+if (opr == "+") {
+  result = num1 + num2;
+} else if (opr == "-") {
+    result = num1 - num2;
+} else if (opr == "*") {
+    result = num1 * num2;
+} else if (opr == "/") {
+    result = num1 / num2;
+}
+alert ("Answer:" + " " + result);
+alert (T)
